91f22aa7ff5e5a12dcc8e3b9f583b7f57942618a
Christopher <christopherjocizz@gmail.com>
2025-03-05 09:27:03 -0500

    Actualizacion de js y arreglo de ruta para imagenes

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// 1.- Crear un array para almacenar los nombres
+let nombres = [];
+// 2.- Implementa una función para agregar amigos
+function agregarAmigos(){
+    let input = document.getElementById("amigo");
+}
