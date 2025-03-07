80b29984159f0665d3902fa7b60eae7964a6cf4b
Christopher <christopherjocizz@gmail.com>
2025-03-07 12:01:02 -0500

    Actualización Final

--- app.js
+++ app.js
@@ -16,10 +16,10 @@ function agregarAmigo (){
     return;
 }
 //Actualizar el array
-    amigos.push(agregarAmigo);
+    amigos.push(nombreAmigo);
     
 //Limpiar el campo de entrada
-agregarAmigo.value = "";
+inputAmigo.value = "";
 //Actualizar el array en el index.html y ver los nombres que se agregan en el prompt
 actualizarLista()
 }
@@ -32,15 +32,23 @@ function actualizarLista (){
 for (let amigo of amigos){
     let li = document.createElement("li");
     li.textContent = amigo;
-    lista.appendChild(li);
+    listaAmigos.appendChild(li);
 }
 }
 //Función para sortear los amigos
 function sortearAmigo(){
 //Validar que haya amigos disponibles
-if (amigos.length === 0){//Se comprueba que el array no este vacio
+if (amigos.length < 2){//Se comprueba que el array no este vacio
     alert ("No ingreso ningun nombre. Por favor ingrese al menos 2 nombres para el sorteo ");
     return;
 }
 //Gererar un índice aleatorio
+let indiceAleatorio = Math.floor (Math.random()*amigos.length); /*
+Math.random() genera un número decimal entre 0 y 1 (ejemplo: 0.7632).
+Math.random() * frutas.length multiplica ese número por la cantidad de elementos en el array (si hay 4 elementos, será entre 0 y 3.999).
+Math.floor() redondea hacia abajo para obtener un número entero válido (entre 0 y 3).*/
+let nombreSorteado = amigos[indiceAleatorio]; // Obtener el nombre sorteado:
+//Mostrar el resultado
+let resultado = document.getElementById("resultado");
+resultado.innerHTML = `Amigo sorteado: ${nombreSorteado}`;
 }
